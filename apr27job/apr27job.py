fff8870a47cb9d65e0d8ec464eb595be4ee4f724
irfan0786hussain <irfan0786hussain@gmail.com>
2025-04-28 01:24:31 +0530

    Synchronizing job apr27job from AWS Glue at 2025/04/27 19:54:30

---
+++ apr27job/apr27job.py
@@ -0,0 +1,21 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+
+df=spark.read.format("csv").option("header","True").option("path","s3://april23jan/Excel.csv").load()
+df.write.format("csv").option("header","True").option("path","s3://april23jan/output").save()
+
+job.commit()
